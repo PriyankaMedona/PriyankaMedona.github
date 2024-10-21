364e8ba507a8fb190f61f51084041d87b82648a9
PriyankaMedona <141740006+PriyankaMedona@users.noreply.github.com>
2024-10-21 06:50:00 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function openResume() {
   // Replace 'your-resume-link' with your actual Google Drive PDF link
-  window.open('https://drive.google.com/file/d/1le8VJ8UACmckNGQJZXiSwIo-brL41tVb/view?usp=sharing', '_blank');
+  window.open('https://drive.google.com/file/d/1JDZR3uR5i-k_SeZXdIEOkS41CaGhJMEh/view?usp=sharing', '_blank');
 }
 
 
