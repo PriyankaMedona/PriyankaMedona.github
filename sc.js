3f5c634d188db022a6a30f862a593e3143b7c8b9
PriyankaMedona <141740006+PriyankaMedona@users.noreply.github.com>
2024-10-21 06:46:27 +0530

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,4 +1,4 @@
 function openResume() {
   // Replace 'your-resume-link' with your actual Google Drive PDF link
-  window.open('https://drive.google.com/your-resume-link', '_blank');
+  window.open('https://drive.google.com/file/d/1JDZR3uR5i-k_SeZXdIEOkS41CaGhJMEh/view?usp=drive_link', '_blank');
 }
