ae84f3174e943d12092d1436cefaec607c0a6dfa
PriyankaMedona <141740006+PriyankaMedona@users.noreply.github.com>
2024-11-14 06:02:07 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,10 @@
-// Wait for the page to load completely
+
 document.addEventListener("DOMContentLoaded", function() {
-  // Select the intro text elements
   const introText = document.querySelector('.intro-text');
-  
-  // Apply a class to trigger animation
   introText.classList.add('animate-intro-text');
 });
-
-
 function openResume() {
-  // Replace 'your-resume-link' with your actual Google Drive PDF link
-  window.open('https://drive.google.com/file/d/1JDZR3uR5i-k_SeZXdIEOkS41CaGhJMEh/view?usp=sharing', '_blank');
+  window.open('https://drive.google.com/file/d/1fKemqHhz8YQsMpxmn4vNElSR3d88Nk1X/view?usp=sharing', '_blank');
 }
 
 
